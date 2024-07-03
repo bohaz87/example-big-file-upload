09cfc3f1c9bd1b6b7af3faaba44c8c9c742fc662
Bob Zhong <sbob@qq.com>
2024-07-03 21:13:17 +0800

    user MD5 hash to check if file alreasy exists

---
+++ public/upload.js
@@ -0,0 +1,119 @@
+/**
+ * @param {File} file
+ * @param {number} SliceSize
+ */
+function getSlice(file, sliceSize = 50 * 1024 * 1024) {
+  const arr = [];
+  let i = 0;
+  while (i * sliceSize < file.size) {
+    arr.push(file.slice(i * sliceSize, (i + 1) * sliceSize));
+    i++;
+  }
+  return arr;
+}
+
+/**
+ *
+ * @param {Blob} fileSlice
+ * @param {number} total
+ * @param {number} idx
+ * @param {string} name
+ * @returns  {Promise<any>}
+ */
+function uploadSlice(
+  fileSlice,
+  { totalSlices, sliceIndex, fileName, fileHash }
+) {
+  const formData = new FormData();
+  formData.append("name", fileName);
+  formData.append("idx", sliceIndex);
+  formData.append("total", totalSlices);
+  formData.append("hash", fileHash);
+  formData.append("file", fileSlice);
+  return fetch("/upload", {
+    method: "POST",
+    body: formData,
+  });
+}
+
+/**
+ * @param {File} file
+ * @param {number|boolean} concurrent
+ */
+async function upload(file, concurrent = false) {
+  const slices = getSlice(file);
+  if (concurrent === false) {
+    concurrent = 1;
+  } else if (concurrent === true) {
+    concurrent = slices.length;
+  }
+
+  const hash = await getHash(slices);
+  const { exist, uri } = await checkFileExist(hash);
+  if (exist) {
+    console.log("file already exsits, upload success");
+    return;
+  }
+
+  const total = slices.length;
+  let start = 0;
+  while (start <= total) {
+    await Promise.all(
+      slices.slice(start, start + concurrent).map((blob, idx) =>
+        uploadSlice(blob, {
+          totalSlices: total,
+          sliceIndex: start + idx,
+          fileName: file.name,
+          fileHash: hash,
+        })
+      )
+    );
+    start += concurrent;
+  }
+}
+
+/**
+ *  get hash for all slices
+ * @param {Blob[]} slices
+ */
+function getHash(slices) {
+  const { promise, resolve, reject } = Promise.withResolvers();
+  const fileReader = new FileReader();
+  const spark = new SparkMD5.ArrayBuffer();
+  let currentChunk = 0;
+
+  fileReader.onload = function (e) {
+    console.log("read chunk nr", currentChunk + 1, "of", slices.length);
+    spark.append(e.target.result); // Append array buffer
+    currentChunk++;
+
+    if (currentChunk < slices.length) {
+      loadNext();
+    } else {
+      console.log("finished loading");
+      const hash = spark.end();
+      console.info("computed hash", hash); // Compute hash
+      resolve(hash);
+    }
+  };
+
+  fileReader.onerror = function (err) {
+    console.err("oops, something went wrong.");
+    reject(err);
+  };
+
+  function loadNext() {
+    fileReader.readAsArrayBuffer(slices[currentChunk]);
+  }
+
+  loadNext();
+  return promise;
+}
+
+async function checkFileExist(hash) {
+  const data = await fetch(`/upload/exists?hash=${hash}`, {}).then((res) =>
+    res.json()
+  );
+
+  return data;
+}
--- server.mjs
+++ server.mjs
@@ -5,35 +5,92 @@ import path from "path";
 import { promisify } from "util";
 
 const __dirname = import.meta.dirname;
+/**
+ * Store file temp path
+ * @type {Record<string, string[]>}
+ */
+let g_files = {};
+let g_hashes = {};
 
 http
   .createServer((req, res) => {
-    console.log(req.url);
-    if (req.url === "/") {
-      fs.readFile("./public/index.html", (err, data) => {
-        if (err) {
-          res.writeHead(500, String(err));
-          res.end();
-          return;
-        }
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      });
-    } else if (req.url === "/upload") {
-      return handleUpload(req, res);
+    console.log("req.url", req.url);
+
+    if (req.url === "/" || req.url.startsWith("/public")) {
+      return staticHandler(req, res);
+    } else if (/^\/upload\/exists/.test(req.url) && req.method === "GET") {
+      return checkFileExistHandler(req, res);
+    } else if (/^\/upload/.test(req.url) && req.method === "POST") {
+      return uploadHandler(req, res);
     } else {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.write("Not Found");
-      res.end();
+      return notFoundHandler(req, res);
     }
   })
   .listen(3000, () => {
     console.log("Server running at http://127.0.0.1:3000/");
   });
 
-let g_fields = {};
-function handleUpload(req, res) {
+/**
+ *
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function notFoundHandler(req, res) {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.write("Not Found");
+  res.end();
+}
+
+function staticHandler(req, res) {
+  let filename = req.url;
+  if (filename === "/") {
+    filename = "/public/index.html";
+  }
+  const file = path.join(__dirname, filename);
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      return notFoundHandler(req, res);
+    }
+    res.writeHead(200);
+    res.write(data);
+    res.end();
+  });
+}
+
+/**
+ *
+ * @param {string} uri
+ */
+function getSearchParams(uri) {
+  const params = {};
+  const query = uri.split("?")[1];
+  query.split("&").forEach((pair) => {
+    const [key, value] = pair.split("=");
+    params[key] = value;
+  });
+  return params;
+}
+
+/**
+ *
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function checkFileExistHandler(req, res) {
+  const params = getSearchParams(req.url);
+  const hash = params.hash;
+  if (hash in g_hashes) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ exist: true, uri: g_hashes[hash] }));
+    res.end();
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ exist: false }));
+    res.end();
+  }
+}
+
+function uploadHandler(req, res) {
   if (
     req.method === "POST" &&
     req.headers["content-type"].startsWith("multipart/form-data")
@@ -50,35 +107,24 @@ function handleUpload(req, res) {
       const name = fields.name[0];
       const idx = Number(fields.idx[0]);
       const total = Number(fields.total[0]);
-      if (idx === 0) {
-        g_fields[name] = Array(total);
-        g_fields[name][idx] = files.file[0].path;
-      } else {
-        g_fields[name][idx] = files.file[0].path;
-      }
-
-      console.log(g_fields);
+      const hash = fields.hash[0];
+      g_files[name] ??= Array(total);
+      g_files[name][idx] = files.file[0].path;
+      const newFileName = hash + path.extname(name);
+      g_hashes[hash] = path.join("/files/", newFileName);
 
       if (idx === total - 1) {
-        await joinFileWithStream(g_fields[name], name);
+        await joinFileWithStream(g_files[name], newFileName);
       }
       // Process fields and files
       res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ success: "true" }));
+      res.end(
+        JSON.stringify({
+          success: "true",
+          url: `/files/${newFileName}`,
+        })
+      );
     });
-    // let data = "";
-
-    // // Collect data chunks
-    // req.on("data", (chunk) => {
-    //   data += chunk;
-    // });
-
-    // req.on("end", () => {
-    //   // Parse the multipart/form-data
-    //   console.log(data);
-    //   res.writeHead(200, { "Content-Type": "application/json" });
-    //   res.end(JSON.stringify({ success: "true" }));
-    // });
   } else {
     res.writeHead(405, { "Content-Type": "text/plain" });
     res.end("Method Not Allowed");
@@ -112,11 +158,10 @@ async function joinFileWithStream(fileList, destFileName) {
       console.log("files have been joined successfullly");
       return;
     }
-    console.log(fileList.length, index);
     const readStream = fs.createReadStream(fileList[index]);
     readStream.pipe(writeStream, { end: false });
     readStream.on("end", () => {
-      delete g_fields[destFileName];
+      delete g_files[destFileName];
       pipeNextFile(index + 1);
     });
   }
